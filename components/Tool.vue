4c87dcb80638ad11858d1fc8afe0db66df31279f
john <johnbardera@gmail.com>
2019-08-18 18:00:43 +0900

    Pin.vue追加,Vueの記法キモい

--- components/Tool.vue
+++ components/Tool.vue
@@ -3,6 +3,7 @@
     building(v-else-if="attr.type === 'building'" :id="id" :attr="attr")
     building-basic(v-else-if="attr.type === 'building_basic'" :id="id" :attr="attr")
     direction(v-else-if="attr.type === 'direction'" :id="id" :attr="attr")
+    pin(v-else-if="attr.type === 'pin'" :id="id" :attr="attr")
 </template>
 
 <script>
@@ -10,12 +11,15 @@ import Example from '~/components/map_tools/Example.vue'
 import Building from '~/components/map_tools/Building.vue'
 import BuildingBasic from '~/components/map_tools/BuildingBasic.vue'
 import Direction from '~/components/map_tools/Direction.vue'
+import Pin from '~/components/map_tools/Pin.vue'
 
 export default {
     components: {
         Example,
-        Building, BuildingBasic,
+        Building,
+        BuildingBasic,
         Direction,
+        Pin
     },
     props: {
         id: {
--- components/map_tools/Pin.vue
+++ components/map_tools/Pin.vue
@@ -4,6 +4,7 @@
         :cx="attr.x" :cy="attr.y"
         @dblclick.stop="select"
         @mousedown.stop="grab"
+        stroke-width="2"
         :style="{stroke: color}"
     )
 </template>
@@ -14,7 +15,7 @@
     export default {
         data() {
             return {
-                type: 'circle',
+                type: 'pin',
             }
         },
         mixins: [BaseTool]
