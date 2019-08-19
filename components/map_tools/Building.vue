6a380c147128eb0fd453a65db101f0bfa0c68dac
Kazuya Izumi <shikibu9419@gmail.com>
2019-08-19 10:58:42 +0900

    Improve building tool to plot freely

--- components/map_tools/Building.vue
+++ components/map_tools/Building.vue
@@ -1,21 +1,33 @@
 <template lang="pug">
-    polygon.tool(
-        v-bind="attr"
-        @dblclick.stop="select"
-        @mousedown.stop="grab"
-        stroke-width="2"
-        :style="{stroke: color}"
-    )
+    g(@dblclick.stop="select")
+        polygon.tool(:points="points" stroke-width="3" stroke="red" v-if="attr.points.length")
+        g(v-if="selected")
+            plot-circle(v-for="(point, index) in attr.points" :key="index"
+                :id="id" :attr="point" :stroke="'red'" :index="index")
+            plot-circle(v-if="plotting"
+                :id="id" :attr="attr"  :stroke="'red'" :now-plotted="true")
 </template>
 
 <script>
 import BaseTool from './BaseTool.vue'
+import PlotCircle from './PlotCircle.vue'
 
 export default {
+    components: {
+        PlotCircle
+    },
     data() {
         return {
             type: 'building',
         }
+    },
+    computed: {
+        plotting() {
+            return this.$store.state.plotting
+        },
+        points() {
+            return this.attr.points.map((point) => point.x + ',' + point.y).join(' ')
+        },
     },
     mixins: [BaseTool]
 }
