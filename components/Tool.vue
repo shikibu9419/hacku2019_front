4188cda67472626e618d044ce67f8e56d33894d4
Kazuya Izumi <shikibu9419@gmail.com>
2019-08-19 10:40:50 +0900

    Fix conflict

--- components/Tool.vue
+++ components/Tool.vue
@@ -1,12 +1,8 @@
 <template lang="pug">
     example(v-if="attr.type === 'circle'" :id="id" :attr="attr")
-<<<<<<< HEAD
     building(v-else-if="attr.type === 'building'" :id="id" :attr="attr")
     building-basic(v-else-if="attr.type === 'building_basic'" :id="id" :attr="attr")
-||||||| merged common ancestors
-=======
     direction(v-else-if="attr.type === 'direction'" :id="id" :attr="attr")
->>>>>>> Add direction tool
 </template>
 
 <script>
@@ -18,7 +14,7 @@ import Direction from '~/components/map_tools/Direction.vue'
 export default {
     components: {
         Example,
-        Building, BuildingBasic
+        Building, BuildingBasic,
         Direction,
     },
     props: {
