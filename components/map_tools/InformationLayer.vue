77ca5a6855615fd7441a8c0d6bece28ff30cbadc
john <johnbardera@gmail.com>
2019-08-21 11:05:42 +0900

    InformationLayer動作確認

---
+++ components/map_tools/InformationLayer.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    rect.information-layer(
+        v-bind:class="{active: !enable}"
+    )
+</template>
+
+<script>
+    export default {
+        name: "InformationLayer.vue",
+        props: {
+            enable: Boolean
+        },
+        created() {
+            this.timeoutDisable()
+        },
+        methods: {
+            timeoutDisable() {
+                setTimeout(_ => {
+                    console.log(this.enable)
+                }, 5000)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .information-layer {
+        height: 100px;
+        width: 100px;
+        background-color: black;
+    }
+    .active {
+        display: none;
+    }
+</style>
--- components/map_tools/Pin.vue
+++ components/map_tools/Pin.vue
@@ -6,18 +6,36 @@
         @mousedown.stop="grab"
         stroke-width="2"
         :style="{stroke: color}"
+        v-on:click="enable(true)"
     )
+        information-layer(
+            v-bind:enable.sync="informationLayerEnable"
+        )
 </template>
 
 <script>
     import BaseTool from './BaseTool.vue'
+    import InformationLayer from './InformationLayer'
 
     export default {
         data() {
             return {
                 type: 'pin',
+                informationLayerEnable: false
             }
         },
-        mixins: [BaseTool]
+        mixins: [BaseTool],
+        components: {
+            InformationLayer
+        },
+        methods: {
+            enable(bool) {
+                this.informationLayerEnable = bool;
+            }
+        }
     }
 </script>
+
+<style>
+
+</style>
