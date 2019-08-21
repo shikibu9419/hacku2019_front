4e64faa55c8fb3c2d0332af2303a62fdd09538fc
Rei Okumura <me1802@s.akashi.ac.jp>
2019-08-21 21:13:35 +0900

    ツールバー以外こんな感じ？

--- pages/home.vue
+++ pages/home.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
     .container
-        card-tile(
-            :title="title"
-            :img_url="img_url"
-            :username="username"
-            :usericon_url="usericon_url"
-            :layers="layers"
-            :tags="tags"
-            :comments="comments"
-        )
+        .card-pages
+            card-tile(
+                v-for="(data, index) in array" :key="`card_test_${index}`"
+                :title="title"
+                :img_url="img_url"
+                :username="username"
+                :usericon_url="usericon_url"
+                :layers="layers"
+                :tags="tags"
+                :comments="comments"
+                :id="data"
+            )
 </template>
 
 <script>
@@ -24,7 +27,8 @@
                 usericon_url: 'https://placehold.jp/100x100.png',
                 layers: ['豚骨', '塩', '醤油', '味噌'],
                 tags: ['ラーメン', '大阪', '激戦区'],
-                comments: [...Array(16).keys()]
+                comments: [...Array(16).keys()],
+                array: [...Array(9).keys()]
             }
         }
     }
@@ -33,5 +37,11 @@
 <style scoped>
     .container {
         background: #eee;
+    }
+    .card-pages{
+        padding: 2.5% 0;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
     }
 </style>
