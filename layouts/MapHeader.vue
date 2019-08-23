5dc18fb4d07842dac56252cbb849264c1c6ee289
Kazuya Izumi <shikibu9419@gmail.com>
2019-08-23 20:59:31 +0900

    Unite component for header of map

--- layouts/MapHeader.vue
+++ layouts/MapHeader.vue
@@ -1,13 +1,58 @@
 <template lang="pug">
     .container
-        map-header
+        .header.map__header
+            h2.map__header--molecules this is MapHeader.vue
+            input.map__header--molecules(v-model="query" placeholder="マップを検索する")
+            button.map__header--molecules(@click="searchOnMap") 検索
         nuxt
 </template>
 
 <script>
-    export default {
-        components: {
-            MapHeader: () => import('~/components/organisms/MapHeader')
+export default {
+    components: {
+    },
+    data () {
+        return {
+            query: ''
         }
-    }
+    },
+    methods: {
+        searchOnMap () {
+            const url = 'https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder'
+
+            this.$jsonp(url, {appid: process.env.YOLP_APPID, query: this.query, output: 'json'})
+                .then(response => {
+                    if (response.ResultInfo.Count === 0) return
+                    for (const feature of response.Feature) {
+                        // get latlng
+                        const latlng = feature.Geometry.Coordinates.split(',').map(c => parseFloat(c)).reverse()
+                        this.$store.dispatch('mapEdit/setMarkerLatLngs', latlng)
+                    }
+                })
+//             var geocoder = new google.maps.Geocoder();
+//             geocoder.geocode({'address': this.query}, function(results, status) {
+//                 if (status === 'OK') {
+//                     console.log(results)
+//                     //                     resultsMap.setCenter(results[0].geometry.location);
+//                     //                     var marker = new google.maps.Marker({
+//                     //                         map: resultsMap,
+//                     //                         position: results[0].geometry.location
+//                     //                     });
+//                 } else {
+//                     alert('Geocode was not successful for the following reason: ' + status);
+//                 }
+//             });
+        }
+    },
+}
 </script>
+
+<style lang="scss">
+.map__header {
+    height: 50px;
+
+    &--molecules {
+        display: inline-block;
+    }
+}
+</style>
