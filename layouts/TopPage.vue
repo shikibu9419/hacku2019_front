457ebb2af8e89ed05527663a0fe2275846160beb
chige12 <tige1215.technic@gmail.com>
2019-08-23 23:47:38 +0900

    TopPageのborderの角丸

--- layouts/TopPage.vue
+++ layouts/TopPage.vue
@@ -2,8 +2,12 @@
   .top_page
     .top_page__border
       .top_page__border__square
-      .top_page__border__circle(v-for="(cor,cur_id) in corner" :key="`top_page_cur_${cur_id}`")
-        div(:class="`top_page__border__circle__${cor}`")
+      .top_page__border__circle(
+        v-for="(cor,cur_id) in corner"
+        :key="`top_page_cur_${cur_id}`"
+        :class="`top_page__border__circle__${cor}`"
+        )
+      .top_page__border__square_white
     nuxt.top_page__inside
 </template>
 
@@ -25,10 +29,17 @@ export default {
 <style lang="scss" scoped>
 @import "~/assets/styles/variables.scss";
 
+$border-margin: 20px;
+$square-width: calc(100% - (20px * 2));
+$border-width: 4px;
+$corner-rounded: 30px;
+$border-circle-margin: $border-margin - $corner-rounded + ($border-width/2);
+
 .top_page {
   position: relative;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
+  overflow: hidden;
 }
 .top_page__border {
   position: absolute;
@@ -44,12 +55,48 @@ export default {
   right: 0;
   bottom: 0;
   margin: auto;
-  width: calc(100% - 60px);
-  height: calc(100% - 60px);
-  border: 4px solid $theme-pink;
+  width: $square-width;
+  height: $square-width;
+  border: $border-width solid $theme-pink;
+}
+.top_page__border__square_white {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  width: 100%;
+  height: 100%;
+  border: $border-margin solid $white;
+}
+
+.top_page__border__circle {
+  position: absolute;
+  width: $corner-rounded*2;
+  height: $corner-rounded*2;
+  background: $white;
+  border-radius: $corner-rounded;
+  border: $border-width solid $theme-pink;
+  &__top_left {
+    top: $border-circle-margin;
+    left: $border-circle-margin;
+  }
+  &__top_right {
+    top: $border-circle-margin;
+    right: $border-circle-margin;
+  }
+  &__bottom_left {
+    bottom: $border-circle-margin;
+    left: $border-circle-margin;
+  }
+  &__bottom_right {
+    bottom: $border-circle-margin;
+    right: $border-circle-margin;
+  }
 }
 
 .top_page__inside {
-  padding: 34px;
+  padding: $border-margin + $border-width;
 }
 </style>
