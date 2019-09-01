df306b8f985a6c09c8842267ff0c2835d7af2fb9
chige12 <tige1215.technic@gmail.com>
2019-09-01 15:41:44 +0900

    MapListコンポーネントをトップのカード表示に使う

--- components/organisms/topPage/MapList.vue
+++ components/organisms/topPage/MapList.vue
@@ -1,14 +1,47 @@
 <template lang="pug">
   .maplists
-    .maplist(v-for="(list,list_id) in lists" :key="`maplist_${list_id}`")
+    CardTile(
+        v-for="(mapCard, index) in 10" :key="`card_test_${index}`"
+        :mapCard="mapCards"
+    )
 </template>
 <script>
 export default {
-  props:['lists'],
-  data(){
-    return{
-
+  components:{
+    CardTile: () => import("~/components/molecules/CardTile"),
+  },
+  data() {
+    return {
+      //VueXから取得する
+      //Likeはlikeしたuserの配列でもいいかも。その辺は任せた！
+      mapCards: {
+        map_id: 'maps',
+        title: '俺的ラーメン屋まとめ',
+        author: 'tanakataro',
+        editors: ['tanakajiro','tanakasaburo','tanakasiro'],
+        img_url: 'https://picsum.photos/400/300',
+        layers: [{id: 1, name: '豚骨'}, {id: 2, name: '塩'}, {id: 3, name: '醤油'}, {id: 4, name: '味噌'}],
+        tags: [{id: 1, name: 'ラーメン'}, {id: 2, name: '大阪'}, {id: 3, name: '激戦区'}],
+        like: 8,
+        stock: 12,
+        comments: [
+          {user:'tanakataro',comment:'わーい'},
+          {user:'tanakataro',comment:'わーい'},
+          {user:'tanakataro',comment:'わーい'}
+        ],
+      }
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+@import "~/assets/styles/variables.scss";
+.maplists {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  overflow-y: auto;
+  padding: 4px 16px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <template lang="pug">
-  .home
+  .home.toppage
     Header(
     :returnLink="'/maplists/search'"
     :placeholder="'マップを検索する'"
     :type="'maplists/search'"
     :buttons="false"
     )
-    .card-pages
-      card-tile(
-          v-for="(mapCard, index) in 10" :key="`card_test_${index}`"
-          :mapCard="mapCards"
-      )
+    main.main
+      SideMenu
+      MapList(lists="searchData")
 </template>
 
 <script>
@@ -20,29 +18,8 @@ export default {
   middleware: 'authenticated',
   components: {
     Header: () => import("~/components/organisms/Header.vue"),
-    CardTile: () => import("~/components/molecules/CardTile")
-  },
-  data() {
-    return {
-      //VueXから取得する
-      //Likeはlikeしたuserの配列でもいいかも。その辺は任せた！
-      mapCards: {
-        map_id: 'maps',
-        title: '俺的ラーメン屋まとめ',
-        author: 'tanakataro',
-        editors: ['tanakajiro','tanakasaburo','tanakasiro'],
-        img_url: 'https://picsum.photos/400/300',
-        layers: [{id: 1, name: '豚骨'}, {id: 2, name: '塩'}, {id: 3, name: '醤油'}, {id: 4, name: '味噌'}],
-        tags: [{id: 1, name: 'ラーメン'}, {id: 2, name: '大阪'}, {id: 3, name: '激戦区'}],
-        like: 8,
-        stock: 12,
-        comments: [
-          {user:'tanakataro',comment:'わーい'},
-          {user:'tanakataro',comment:'わーい'},
-          {user:'tanakataro',comment:'わーい'}
-        ],
-      }
-    }
+    SideMenu: () => import("~/components/organisms/topPage/SideMenu.vue"),
+    MapList: () => import("~/components/organisms/topPage/MapList.vue")
   }
 }
 </script>
@@ -50,11 +27,15 @@ export default {
 <style lang="scss" scoped>
 @import "~/assets/styles/variables.scss";
 
-.card-pages{
-  padding: 2.5% 0;
+.toppage {
+  width: 100%;
+  height: 100vh;
+}
+
+.main {
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+  width: 100%;
+  height: $main-height;
   background: $back-light-gray;
 }
 </style>
