4964c95bc4a810f971a5164b6ae863b9143fc9ce
chige12 <tige1215.technic@gmail.com>
2019-09-03 03:25:12 +0900

    サーチ結果を持ってくるパーサー（途中）誰かやってほしい

--- components/organisms/topPage/MapList.vue
+++ components/organisms/topPage/MapList.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
   .maplists
     CardTile(
-        v-for="(mapCard, index) in 10" :key="`card_test_${index}`"
-        :mapCard="mapCards"
+        v-for="(mapCard, index) in mapDatas" :key="`card_test_${index}`"
+        :mapCard="mapCard"
     )
+    .maplists--none(
+      v-if="mapDatas.length===0"
+    ) {{maplist_none_text}}
 </template>
 <script>
 export default {
@@ -14,11 +17,11 @@ export default {
     return {
       //VueXから取得する
       //Likeはlikeしたuserの配列でもいいかも。その辺は任せた！
-      mapCards: {
+      mapCards: [{
         map_id: 'maps',
         title: '俺的ラーメン屋まとめ',
-        author: 'tanakataro',
-        editors: ['tanakajiro','tanakasaburo','tanakasiro'],
+        author: 'tanakataro',//1
+        editors: ['tanakajiro','tanakasaburo','tanakasiro'],//2,3
         img_url: 'https://picsum.photos/400/300',
         layers: [{id: 1, name: '豚骨'}, {id: 2, name: '塩'}, {id: 3, name: '醤油'}, {id: 4, name: '味噌'}],
         tags: [{id: 1, name: 'ラーメン'}, {id: 2, name: '大阪'}, {id: 3, name: '激戦区'}],
@@ -29,6 +32,44 @@ export default {
           {user:'tanakataro',comment:'わーい'},
           {user:'tanakataro',comment:'わーい'}
         ],
+      }],
+    }
+  },
+  computed:{
+    mapDatas(){
+      let serve_maps = this.$store.state.maplist.maps
+      let map_datas = []
+      if(this.$route.path == '/'){
+        map_datas = this.mapCards
+      }
+      if(serve_maps.length !== 0){
+        for (let i = 0; i < serve_maps.length; i++) {
+          let sMap = serve_maps[i]
+          let fMap = {}
+          fMap.map_id = sMap.map_id
+          fMap.title = sMap.map_name
+          fMap.author = sMap.permission[0].user_name
+          //途中で投げた。誰かお願いします。
+          map_datas.push()
+        }
+      }
+      return map_datas
+    },
+    maplist_none_text(){
+      switch(this.$route.path){
+        case '/' :
+        case '/maplists/search' :
+          return "検索結果はありません。"
+          break;
+        case '/maplists/mymap' :
+          return "マイマップは無いようです。"
+          break;
+        case '/maplists/stock' :
+          return "ストックしたマップは無いようです。"
+          break;
+        case '/maplists/like' :
+          return "いいねしたマップは無いようです。"
+          break;
       }
     }
   }
@@ -36,12 +77,19 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "~/assets/styles/variables.scss";
+@import "~/assets/styles/mixin.scss";
 .maplists {
   width: 100%;
   height: 100%;
   display: flex;
   flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
   overflow-y: auto;
   padding: 4px 16px;
+}
+.maplists--none {
+  padding: 8px;
+  @include noto-font(1.8rem);
 }
 </style>
