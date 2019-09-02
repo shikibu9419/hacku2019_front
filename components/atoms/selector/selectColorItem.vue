adc1d23e84c928c8f9622591eb0f60e0bfa6fc86
john <johnbardera@gmail.com>
2019-09-02 22:30:12 +0900

    add selectColorItem

--- components/atoms/selector/selectColorItem.vue
+++ components/atoms/selector/selectColorItem.vue
@@ -1,16 +1,13 @@
 <template lang="pug">
     .select-color-item
         LayerIcon.select-color-item__icon(v-bind:class="[item.value]")
-        img.select-color-item__icon(src="~/assets/svgs/layer.svg?data" v-bind:class="[item.value]")
+        slot
 </template>
 
 <script>
     import LayerIcon from '~/assets/svgs/layer.svg?inline'
     export default {
         name: "selectColorItem.vue",
-        modules: [
-            '@nuxtjs/svg'
-        ],
         components: {
             LayerIcon
         },
@@ -27,26 +24,14 @@
 
 <style lang="scss" scoped>
     @import "../../../assets/styles/variables";
+    @import "../../../assets/styles/atoms/layerSvg";
     .select-color-item {
+        height: 32px;
+        display: flex;
+        align-items: center;
         &__icon {
-            width: 20px;
-        }
-        .light-pink {
-            color: $palette-light-pink;
-        }
-        .pink {
-            color: $palette-pink;
-        }
-        .red {
-            color: $palette-red;
-            fill: $palette-red;
-            stroke: red;
-        }
-        .light-orange {
-            color: $palette-light-orange;
-        }
-        .orange {
-            color: $palette-orange;
+            width: 24px;
+            margin-right: 8px;
         }
     }
 </style>
