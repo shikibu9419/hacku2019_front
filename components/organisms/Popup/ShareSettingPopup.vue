b3600b36a93b9d6f70d6e05904d53d0d36262ec2
chige12 <tige1215.technic@gmail.com>
2019-09-03 11:12:28 +0900

    Share Modal Style 1

--- components/organisms/Popup/ShareSettingPopup.vue
+++ components/organisms/Popup/ShareSettingPopup.vue
@@ -4,26 +4,29 @@
             DeleteAndCloseButton(v-bind:deleteHandler="closeModal" v-bind:closeHandler="closeModal")
         .share-setting-popup__setting
             .share-setting-popup__link-wrapper 公開リンク
-                .share-setting-popup__link {{ param.link }}
+                .share-setting-popup__link
+                    .share-setting-popup__link__url {{ param.link }}
                     .share-setting-popup--item-end-button コピーする
                 .share-setting-popup__logos
-                    .share-setting-popup__icon TwitterLogo
-                    .share-setting-popup__icon FacebookLogo
+                    .share-setting-popup__icon: TwitterBtn.share-setting-popup__icon--svg
+                    .share-setting-popup__icon: FacebookBtn.share-setting-popup__icon--svg
             .share-setting-popup__disclosure-wrapper
-                .share-setting-popup__disclosure-scope
-                    .share-setting-popup__disclosure-scope-name グローバル公開
-                    .share-setting-popup__disclosure-scope-discription 検索でヒットし、誰でもアクセスできます。
-                    button.share-setting-popup__disclosure-scope-toggle 仮置き
-                .share-setting-popup__disclosure-scope
-                    .share-setting-popup__disclosure-scope-name URL公開
-                    .share-setting-popup__disclosure-scope-discription URLを持っている人がアクセスできます。
-                    button.share-setting-popup__disclosure-scope-toggle 仮置き
+                .share-setting-popup__disclosure-scope_list
+                    .share-setting-popup__disclosure-scope
+                        .share-setting-popup__disclosure-scope-name グローバル公開
+                        .share-setting-popup__disclosure-scope-discription 検索でヒットし、誰でもアクセスできます。
+                        button.share-setting-popup__disclosure-scope-toggle 仮置き
+                    .share-setting-popup__disclosure-scope
+                        .share-setting-popup__disclosure-scope-name URL公開
+                        .share-setting-popup__disclosure-scope-discription URLを持っている人がアクセスできます。
+                        button.share-setting-popup__disclosure-scope-toggle 仮置き
                 .share-setting-popup__url-authority 権限
                     Selector(v-bind:items.async="items")
-                .share-setting-popup__disclosure-scope
-                    .share-setting-popup__disclosure-scope-name ユーザーに公開
-                    .share-setting-popup__disclosure-scope-discription 特定のユーザーに対して公開設定を行います。
-                    button.share-setting-popup__disclosure-scope-toggle 仮置き
+                .share-setting-popup__disclosure-scope_list
+                    .share-setting-popup__disclosure-scope
+                        .share-setting-popup__disclosure-scope-name ユーザーに公開
+                        .share-setting-popup__disclosure-scope-discription 特定のユーザーに対して公開設定を行います。
+                        button.share-setting-popup__disclosure-scope-toggle 仮置き
                 .share-setting-popup__user-authority
                     .share-setting-popup__user-searcher-wrapper
                         input.share-setting-popup__user-searcher(placeholder="ユーザーネームまたはメールアドレスで検索")
@@ -46,12 +49,14 @@
         },
         components: {
             DeleteAndCloseButton,
-            Selector
+            Selector,
+            TwitterBtn: () => import('~/assets/svgs/twitter.svg?inline'),
+            FacebookBtn: () => import('~/assets/svgs/facebook.svg?inline'),
         },
         data() {
             return {
                 param: {
-                    link: 'https://yahoo.co.jp'
+                    link: 'https://mille-feuille.app/map_id/view'
                 },
                 items: [
                     { text: '拒否', value: 0, selected: true },
@@ -92,26 +97,52 @@
         &__link {
             display: flex;
             justify-content: space-between;
-            margin-left: 8px;
+            margin: 4px 0;
             border: solid 2px $dark-gray;
+            &__url {
+                padding: 4px;
+            }
         }
         &--item-end-button {
             text-align: center;
-            width: 160px;
+            flex-grow: 0;
+            flex-shrink: 0;
+            width: 140px;
+            padding: 4px;
             left: 0;
             background: $dark-gray;
             color: $white;
+            border-left: 2px solid $dark-gray;
+            cursor: pointer;
+            &:hover{
+                background: $gray;
+            }
         }
         &__logos {
-            margin: 0 0 0 auto;
+            display: flex;
+            flex-wrap: nowrap;
+            justify-content: flex-end;
+        }
+        &__icon {
+            cursor: pointer;
+            width: 30px;
+            height: 30px;
+            &--svg {
+                display: block;
+                width: 30px;
+            }
         }
         &__disclosure-wrapper {
             display: flex;
             flex-direction: column;
             padding: 16px;
         }
+        &__disclosure-scope_list {
+            margin: 12px 0 4px;
+        }
         &__disclosure-scope {
             display: flex;
+            margin: 2px 0;
         }
         &__disclosure-scope-name {
             font-weight: bold;
@@ -130,12 +161,15 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
-            margin-left: 8px;
+            margin: 4px 0;
             border: solid 2px $dark-gray;
         }
         &__user-searcher {
-            width: auto;
-            height: auto;
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 4px;
+            border: none;
         }
         &__user-searcher-wrapper,&__users-added {
             width: 100%;
