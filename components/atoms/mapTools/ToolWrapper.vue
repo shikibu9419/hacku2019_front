82aa84f0992e99186ed0908f3ffcc15f0d755372
john <johnbardera@gmail.com>
2019-09-03 12:53:39 +0900

    add TextToll

--- components/atoms/mapTools/ToolWrapper.vue
+++ components/atoms/mapTools/ToolWrapper.vue
@@ -9,6 +9,8 @@
       :id="id" :attr="attr" :layer-active="layerActive")
   box(v-else-if="attr.type === 'box'"
       :id="id" :attr="attr" :layer-active="layerActive")
+  TextTool(v-else-if="attr.type === 'text'"
+    :id="id" :attr="attr" :layer-active="layerActive")
 </template>
 
 <script>
@@ -21,6 +23,7 @@ export default {
     Direction: () => import('./Direction'),
     Pin: () => import('./Pin'),
     Box: () => import('./Box'),
+    TextTool: () => import('./Text')
   },
   mixins: [Shared]
 }
